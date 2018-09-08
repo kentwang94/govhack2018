863e8da383f0d70a14429fffb27cb97c45e8e489
Zhouyang <kent.wang.anu@gmail.com>
2018-09-09 02:03:07 +1000

    add query for off street parking

--- backend_app/database/scratch.sql
+++ backend_app/database/scratch.sql
@@ -4,3 +4,15 @@ WITH path AS(
 SELECT COUNT(*)
 FROM clean_trees, path
 WHERE ST_DWithin(clean_trees.geom, path.geom, 100, TRUE);
+
+
+WITH destination AS(
+  SELECT ST_LineFromText('') AS geom
+)
+SELECT SUM(parking_spaces) AS total
+FROM clean_off_parking, destination
+WHERE ST_DWithin(clean_off_parking.geom, destination.geom, 200, TRUE)
+GROUP BY parking_spaces;
+
+
+WITH
--- backend_app/routes/index.js
+++ backend_app/routes/index.js
@@ -2,8 +2,12 @@ var express = require('express');
 var router = express.Router();
 const db = require('../database/db.js');
 
-let points2WKT = (pts) => {
+let points2text = (pts) => {
   return "LINESTRING(" + pts.map(x => '' + x.lng + ' ' + x.lat).join(', ') + ')';
+};
+
+let point2text = (pt) => {
+  return "POINT(" + pt.lng + ' ' + pt.lat + ')'
 };
 
 /* GET home page. */
@@ -15,28 +19,42 @@ router.post('/path', (req, res) => {
   let path = req.body['path'];
   let distance = req.body['distance'];
   let resObject = {};
-  db.any(
+  db.one( //Query the number of trees within 100 metres along the path
     "WITH path AS( " +
     "  SELECT ST_LineFromText($1) AS geom " +
     ") " +
     "SELECT COUNT(*) " +
     "FROM clean_trees, path " +
     "WHERE ST_DWithin(clean_trees.geom, path.geom, 100, TRUE)",
-    points2WKT(path)
+    points2text(path)
   ).then(result => {
     let wayPoint = [{lat: -37.81, lng: 144.96}, {lat: -37.80, lng: 144.95}];
     resObject.tree_cnt = result;
     resObject.way_point = wayPoint;
     resObject.toilet = [{lat: -37.8063, lng: 144.9596}];
-    console.log(resObject);
-    res.send(resObject);
+    db.one( //Query the total parking spaces within 250 metres within the destination
+      "WITH destination AS( " +
+      "  SELECT ST_PointFromText($1) AS geom " +
+      ") " +
+      "SELECT SUM(parking_spaces) AS total " +
+      "FROM clean_off_parking, destination " +
+      "WHERE ST_DWithin(clean_off_parking.geom, destination.geom, 250, TRUE) " +
+      "GROUP BY parking_spaces;",
+      point2text(path[path.length-1])
+    ).then(result => {
+      resObject.parking_spaces = result;
+      //db.any().then(result => {});
+
+      console.log(resObject);
+      res.send(resObject);
+    });
   });
 });
 
 
 /**
- * # off street parking
+ * # off street parking, destination, 200 metres
  * # no.trees / total_foot_path_distance
- * # ped_volume
+ * # ped_volume  $$ sensor_list
  */
 module.exports = router;
